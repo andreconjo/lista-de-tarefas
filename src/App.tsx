d95dea38e3f64e0c49f58258c2c4aa716cf2216b
andreconjo <andre.henrique@alu.ufc.br>
2023-01-03 23:39:27 -0300

    Add today system

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import * as C from './App.styles'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Item} from "./types/Item";
 import {ListItem} from "./components/ListItem";
 import {AddArea} from "./components/AddArea";
 import {Footer} from "./components/Footer";
+import {format} from 'date-fns'
 
 const App = () => {
 
-    const [list, setList] = useState<Item[]>([
-    ]);
+    const [list, setList] = useState<Item[]>([]);
+    const [allowToSave, setAllowToSave] = useState<boolean>(false);
 
     const handleAddTask = (taskName: string) => {
         let newList = [...list];
@@ -23,8 +24,8 @@ const App = () => {
 
     const handleCheckItem = (id: number, done: boolean) => {
         let newList = [...list];
-        for(let i in newList) {
-            if(newList[i].id === id) {
+        for (let i in newList) {
+            if (newList[i].id === id) {
                 newList[i].done = done;
             }
         }
@@ -34,13 +35,47 @@ const App = () => {
     const handleOnDelete = (id: number) => {
         let newList = [...list].filter(item => item.id !== id);
         setList(newList);
+        save();
     }
+
+    const getActualDate = () => {
+        return format(new Date(), 'dd/MM/yyyy')
+    }
+
+    const save = () => {
+        let data = {
+            date: getActualDate(),
+            list: [...list]
+        }
+        localStorage.setItem('mytodo', JSON.stringify({...data}))
+    }
+
+    const load = () => {
+        let data = localStorage.getItem('mytodo')
+        if (data) {
+            let dataJson = JSON.parse(data)
+            if (dataJson.date === getActualDate()) {
+                setList(dataJson.list);
+            }
+        }
+
+        setAllowToSave(true);
+    }
+
+    useEffect(() => {
+        load()
+    }, [])
+
+    useEffect(() => {
+        if (allowToSave)
+            save();
+    }, [list, allowToSave])
 
 
     return (
         <C.Container>
             <C.Area>
-                <C.Header>TODO LIST</C.Header>
+                <C.Header>TODO LIST - {format(new Date(), 'dd/MM/yyyy')}</C.Header>
                 <AddArea onEnter={handleAddTask}/>
                 {list.map((item, index) => (
                     <ListItem key={index} item={item}
